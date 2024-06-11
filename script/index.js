8a12ce271bd742b40c10a6ecd2ec06266399c649
Yash28060 <yashkumarsaini2016@gmail.com>
2024-06-11 14:58:45 +0530

    adding changes

--- script/index.js
+++ script/index.js
@@ -31,46 +31,6 @@ function togglePopup() {
   }
 }
 
-// cart section
-const cartOverlay = document.getElementById("cartOverlay");
-function toggleCart() {
-  if (cartOverlay.classList.contains("pointer-events-none")) {
-    cartOverlay.classList.remove("pointer-events-none");
-    setTimeout(() => {
-      cartOverlay.classList.remove("opacity-0", "scale-75");
-    }, 10);
-  } else {
-    cartOverlay.classList.add("opacity-0", "scale-75");
-    setTimeout(() => {
-      cartOverlay.classList.add("pointer-events-none");
-    }, 300);
-  }
-}
-//  or this below function but there is no transition delay in it
-// function toggleCart() {
-//   const classes = ["pointer-events-none", "opacity-0", "scale-75"];
-  
-//   classes.forEach(cls => {
-//     cartOverlay.classList.toggle(cls);
-//   });
-// }
-
-// cartShopoverlay
-const cartShopOverlay = document.getElementById("cartShopOverlay");
-function toggleCartShop() {
-  if (cartShopOverlay.classList.contains("pointer-events-none")) {
-    cartShopOverlay.classList.remove("pointer-events-none");
-    setTimeout(() => {
-      cartShopOverlay.classList.remove("opacity-0", "scale-75");
-    }, 10);
-  } else {  
-    cartShopOverlay.classList.add("opacity-0", "scale-75");
-    setTimeout(() => {
-      cartShopOverlay.classList.add("pointer-events-none");
-    }, 300);
-  }
-}
-
 function toggleForms(formType) {
   const loginForm = document.getElementById('login_form');
   const signUpForm = document.getElementById('signup_form');
@@ -115,46 +75,18 @@ function startCarousel() {
   showSlide(currentIndex);
   setInterval(showNextSlide, 4000);
 }
+startCarousel();  
 
-// carousel for products starts here //
-
-let initialIndex = 0;
-const products = document.querySelectorAll('.prod');
-const productsPerPage = 4;
-
-function showProducts(index) {
-  products.forEach((prod, i) => {
-    prod.classList.add('hidden');
-    if (i >= index && i < index + productsPerPage) {
-      prod.classList.remove('hidden');
-    }
-  });
-}
-
-function showRightSlide() {
-  initialIndex = (initialIndex + 1) % products.length;
-  showProducts(initialIndex);
-}
-
-function showLeftSlide() {
-  initialIndex = (initialIndex - 1 + products.length) % products.length;
-  showProducts(initialIndex);
-}
-
-function showProd() {
-  showProducts(initialIndex);
-  setInterval(showRightSlide, 4000);
-}
-
-showProd();
-
-
-
-window.addEventListener('load', startCarousel);
+// window.addEventListener('load', startCarousel);
 
 // Default to showing login form
 toggleForms('login');
 
-window.onload = function() {
-  setTimeout(togglePopup, 4000); // 5000 milliseconds = 5 seconds
-};
+// window.onload = function() {
+//   setTimeout(togglePopup, 4000); // 5000 milliseconds = 5 seconds
+// };
+window.onload = function(){
+  setTimeout(function(){
+    togglePopup();
+  }, 3000); 
+ };
---
+++ script/product.js
@@ -0,0 +1,72 @@
+let initialIndex = 0;
+const products = document.querySelectorAll('.prod');
+const productsPerPage = 4;
+
+function showProducts(index) {
+  products.forEach((prod, i) => {
+    prod.classList.add('hidden');
+    if (i >= index && i < index + productsPerPage) {
+      prod.classList.remove('hidden');
+    }
+  });
+}
+
+function showRightSlide() {
+  initialIndex = (initialIndex + 1) % products.length;
+  showProducts(initialIndex);
+}
+
+function showLeftSlide() {
+  initialIndex = (initialIndex - 1 + products.length) % products.length;
+  showProducts(initialIndex);
+}
+
+function showProd() {
+  showProducts(initialIndex);
+  setInterval(showRightSlide, 4000);
+}
+
+showProd();
+ 
+// cart section
+const cartOverlay = document.getElementById("cartOverlay");
+function toggleCart() {
+  if (cartOverlay.classList.contains("pointer-events-none")) {
+    cartOverlay.classList.remove("pointer-events-none");
+    setTimeout(() => {
+      cartOverlay.classList.remove("opacity-0", "scale-75");
+    }, 10);
+  } else {
+    cartOverlay.classList.add("opacity-0", "scale-75");
+    setTimeout(() => {
+      cartOverlay.classList.add("pointer-events-none");
+    }, 300);
+  }
+}
+//  or this below function but there is no transition delay in it
+// function toggleCart() {
+//   const classes = ["pointer-events-none", "opacity-0", "scale-75"];
+  
+//   classes.forEach(cls => {
+//     cartOverlay.classList.toggle(cls);
+//   });
+// }
+
+// cartShopoverlay
+const cartShopOverlay = document.getElementById("cartShopOverlay");
+function toggleCartShop() {
+  if (cartShopOverlay.classList.contains("pointer-events-none")) {
+    cartShopOverlay.classList.remove("pointer-events-none");
+    setTimeout(() => {
+      cartShopOverlay.classList.remove("opacity-0", "scale-75");
+    }, 10);
+  } else {  
+    cartShopOverlay.classList.add("opacity-0", "scale-75");
+    setTimeout(() => {
+      cartShopOverlay.classList.add("pointer-events-none");
+    }, 300);
+  }
+}
+
+
+window.addEventListener('load', showProd);
